225b5b9000920083f177746539d39a8c8216ff18
surya-tinus <suryanovenskytinus@gmail.com>
2025-09-14 16:10:09 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -1,12 +1,11 @@
 package com.example.lab_week_03
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.util.Log
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,57 +15,57 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class ListFragment : Fragment(), View.OnClickListener{
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var coffeeListener: CoffeeListener;
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d(TAG, "onAttach")
+        if(context is CoffeeListener){
+            coffeeListener = context
+        }
+        else{
+            throw RuntimeException("Must implement CoffeeListener")
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG,"onCreate")
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.d(TAG,"onCreateView")
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated")
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+        coffeeList.forEach{
+            it.setOnClickListener(this)
+        }
     }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
+    override fun onClick(v: View?) {
+        v?.let{
+                coffee -> coffeeListener.onSelected(coffee.id)
+        }
     }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
-    }
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -74,17 +73,16 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment ListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            ListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-        private const val TAG = "MainFragment"
     }
 }
--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
 import android.content.Context
-class MainActivity : AppCompatActivity() {
+
+interface CoffeeListener {
+    fun onSelected(id: Int)
+}
+class MainActivity : AppCompatActivity(), CoffeeListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,29 +23,12 @@ class MainActivity : AppCompatActivity() {
                 systemBars.bottom)
             insets
         }
-        Log.d(TAG, "onCreate")
     }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    companion object {
-        private const val TAG = "MainActivity"
+    override fun onSelected(id: Int){
+        val detailFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_detail)
+                as DetailFragment
+        detailFragment.setCoffeeData(id)
     }
 }
+
